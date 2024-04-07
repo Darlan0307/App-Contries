33119ed2514f4e1dffc3543f61651ca55423430c
Darlan0307 <darlanchagas2020@gmail.com>
2024-04-07 20:30:57 -0300

    :iphone:feat: componente header responsivo

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -3,8 +3,8 @@ import ModeToggle from "../ModeToggle"
 
 const AppHeader = () => {
   return (
-    <header>
-      <h1>Titulo</h1>
+    <header className="flex flex-wrap justify-center gap-4 items-center py-8 px-10 min-[380px]:justify-between sm:px-20">
+      <h1 className="text-xl sm:text-3xl" >Where is the World?</h1>
       <ModeToggle/>
     </header>
   )
